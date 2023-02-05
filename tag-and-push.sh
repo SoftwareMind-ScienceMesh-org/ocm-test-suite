67f2f2f87c5d609615c1950570b3644baa12ba39
dagl <dagl@softwaremind.com>
2023-02-05 14:48:21 +0000

    update tag-and-push

--- tag-and-push.sh
+++ tag-and-push.sh
@@ -1,17 +1,17 @@
-docker tag apache-php 10.8.2.27:5000/apache-php
-docker push 10.8.2.27:5000/apache-php
+docker tag apache-php-8.0 10.8.2.27:5000/apache-php-8.0
+docker push 10.8.2.27:5000/apache-php-8.0
 docker tag owncloud 10.8.2.27:5000/owncloud
 docker push 10.8.2.27:5000/owncloud
 docker tag oc1 10.8.2.27:5000/oc1
 docker push 10.8.2.27:5000/oc1
 docker tag oc2 10.8.2.27:5000/oc2
 docker push 10.8.2.27:5000/oc2
-docker tag revad 10.8.2.27:5000/revad
-docker push 10.8.2.27:5000/revad
-docker tag ci 10.8.2.27:5000/ci
+#docker tag revad 10.8.2.27:5000/revad
+#docker push 10.8.2.27:5000/revad
+#docker tag ci 10.8.2.27:5000/ci
 docker tag stub 10.8.2.27:5000/stub
 docker push 10.8.2.27:5000/stub
-docker push 10.8.2.27:5000/ci
+#docker push 10.8.2.27:5000/ci
 docker tag nextcloud 10.8.2.27:5000/nextcloud
 docker push 10.8.2.27:5000/nextcloud
 docker tag nc1 10.8.2.27:5000/nc1
